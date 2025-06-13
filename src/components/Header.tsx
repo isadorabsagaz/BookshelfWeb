bc97422519cc83c544c0ebcc8d8d296e94e690ca
isadorabsagaz <isadora.sagaz@gmail.com>
2025-06-13 20:06:31 -0300

    feat(Header): add PDF generator button.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,71 @@
 import "../css/Header.css";
 import bookshelfLogo from "../assets/BookshelfIcon.svg";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "../services/api";
+
 
 const Header = () => {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <header className="app-header">
+  const handleDownloadPDF = async () => {
+    const userId = localStorage.getItem("userId");
+    const token = localStorage.getItem("token");
+    const PDF_URL = `api/user/${userId}/books/pdf`;
 
-                <div className="logo"
-                    onClick={() => navigate("/")}>
+    if (!userId || !token) {
+      alert("User not authenticated!");
+      return;
+    }
 
-                    <img src={bookshelfLogo} alt="bookshlf icon"/>
-                    <p className={"title"}>Bookshelf</p>
+    try {
+      const response = await axios.get(PDF_URL, {
+          responseType: "blob",
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+      });
 
-                </div>
-                <button className={"log-button"}
-                        onClick={() => navigate('/login')}
-                >Log in
-                </button>
-            </header>
+      const blob = new Blob([response.data], {type: 'application/pdf'});
+      const url = window.URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "bookshelf.pdf");
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      window.URL.revokeObjectURL(url);
+
+    } catch (err: any){
+      console.log("Erro ao baixar PDF: ", err.response?.data || err.message);
+      alert("Erro ao baixar o PDF");
+    }
+  };
+
+  return (
+    <div>
+      <header className="app-header">
+
+        <div className="logo"
+             onClick={() => navigate("/")}>
+
+          <img src={bookshelfLogo} alt="bookshlf icon"/>
+          <p className={"title"}>Bookshelf</p>
+
         </div>
-    );
+        <button className={"log-button"}
+                onClick={() => navigate('/login')}
+        >Log in
+        </button>
+        <button className={"log-button"}
+                onClick={handleDownloadPDF}
+        >
+          PDF
+        </button>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
